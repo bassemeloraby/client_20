5ab1c367c15204c24245bf7062081441d4f2cd19
bassem <basem12683@gmail.com>
2023-10-14 22:52:22 +0300

    updates

--- src/components/cosmotic/CosmoticList.js
+++ src/components/cosmotic/CosmoticList.js
@@ -1,11 +1,15 @@
 import { Virtuoso } from "react-virtuoso";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-const CosmoticList = ({ items,setUpdateProduct }) => {
+const l1 = "https://www.google.com/search?q=";
+const l2 =
+  "&sca_esv=571711580&rlz=1C1VDKB_enSA1075SA1075&tbm=isch&sxsrf=AM9HkKkhm2K1JYgiDZSvrn-lnYR52Xi5vA:1696763801819&source=lnms&sa=X&ved=2ahUKEwj18LLdqeaBAxXD2wIHHfNMAzMQ_AUoAXoECAQQAw&biw=1366&bih=641&dpr=1";
+
+const CosmoticList = ({ items, setUpdateProduct }) => {
   const navigate = useNavigate();
 
   const editHandler = (prod) => {
-    setUpdateProduct(prod)
+    setUpdateProduct(prod);
     navigate(`/cosmotics/cosmoticUpdate`);
     console.log(prod);
   };
@@ -34,7 +38,17 @@ const CosmoticList = ({ items,setUpdateProduct }) => {
               {" "}
               <h3>{prod.Description}</h3>
               <h6>{prod.Company} </h6>
-              <h6>{prod.Category} </h6>
+              <h6>
+                {prod.Category}{" "}
+                <Link
+                  to={l1 + prod.Description + l2}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "red" }}
+                >
+                  Google Pic
+                </Link>
+              </h6>
             </div>
             <div>
               <Button variant="success" onClick={() => editHandler(prod)}>
